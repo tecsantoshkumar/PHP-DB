eecf7fab0c4db391c409247f474746af55a1bf1d
tec Santosh Kumar <62633516+tecsantoshkumar@users.noreply.github.com>
2022-05-23 23:36:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,32 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP DATABASE</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+<div class="topnav">
+        <a class="active" href="#">PHP DATABASE</a>
+        <div class="search-container">
+            <form action="/action_page.php">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+<div class="div">
+        <form action="create.php" method="GET">
+            <label for="fname">Name</label>
+            <input class="input" type="text" id="name" name="name" placeholder="Your name..">
 
-    <form action="create.php" method="GET">
-        <label for="name">Name<input type="text" name="name" placeholder="Enter your Name"></label><br>
-        <label for="email">Email <input type="text" name="email" placeholder="Enter your Email"></label><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
+            <label for="lname">Email</label>
+            <input class="input" type="text" id="email" name="email" placeholder="Your  email..">
+            <input class="input" type="submit" name="submit" value="Save">
+        </form>
+    </div>
 
     <table>
         <thead>
@@ -46,8 +61,8 @@ $conn->close();
             <td><?php echo $row["id"]; ?></td>
             <td><?php echo $row["name"]; ?></td>
             <td><?php echo $row["email"]; ?></td>
-            <td><a href="update.php?id=<?php echo $row["id"]; ?>">Edit</a></td>
-            <td><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
+            <td><a href="update.php?id=<?php echo $row["id"]; ?>"><i class='fa fa-edit'  style='font-size:28px;color:blue'></i></a></td>
+            <td><a href="delete.php?id=<?php echo $row["id"]; ?>"><i class='material-icons' style='font-size:28px;color:red'>delete</i></a></td>
         </tr>
 
         <?php }} ?>
